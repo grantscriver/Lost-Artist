8d34780333f1d8a2045814dd50c569559b690952
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-03 14:04:36 -0600

    added server.js and setup scripts in package.json to run express server & react-scripts. Adjusted /client/App.js to include API call to server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `Here's your post request: ${req.body.post}`,
+  );
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
