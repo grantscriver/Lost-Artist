be38c1ac319587b1716636e211cba91d52b56171
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-05 11:04:27 -0600

    changed PORT number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
