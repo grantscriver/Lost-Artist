bc2f2e0046351b60af1012ac6ad22a84da96d311
harms.jared@outlook.com <harms.jared@outlook.com>
2020-12-09 21:03:26 -0600

    uploaded register page and replaced lost code

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,8 +1,18 @@
 import React from 'react';
+// import Navbar from './components/Navbar/Navbar';
 
 function Register() {
     return (
-        <div></div>
+        <section class="hero is-success is-fullheight has-background-grey-light">
+            <div class="hero-body">
+                <div class="container">
+                    <figure class=".upload-img">
+                        <img src="https://images.pexels.com/photos/1868475/pexels-photo-1868475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                    </figure>
+                </div>
+            </div>
+        </section>
+
     )
 }
 
