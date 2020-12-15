eb16972831557e74f96b289bf8e360f76df6a6e0
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-14 20:47:55 -0600

    added connection to firebase to populate data by ID in URL

--- client/src/pages/Shop/itemdetails.js
+++ client/src/pages/Shop/itemdetails.js
@@ -1,16 +1,55 @@
-import React from "react";
-import Hero from "../../components/Hero/Hero";
-import { NavLink } from "react-router-dom";
-import ShopDropDowns from "../../components/ShopDropDowns/ShopDropDowns";
+import React, { useState, useEffect } from "react";
+
+import { NavLink, Link, Route } from "react-router-dom";
 import Menu from "../../components/Menu/Menu";
+// import axios from "axios";
+
+import { app } from "../../base";
+
+const db = app.firestore();
 
 function ItemDetails() {
-    // const [] = useState([]);
+    const [item, setItem] = useState([]);
+    // let pathItem = window.location.pathname;
+    let URL = window.location.pathname;
+    let search = URL.lastIndexOf('/');
+    let resultId = URL.substring(search + 1);
+    
+    // -------- From backend API/database/sequelize -------
     // useEffect(() => {
+    //     let itemId = props.id;
+    //     axios.get("/api/items/" + itemId)
+    //     .then(res => {
+    //         console.log(res);
+    //         setItem(res);
+    //     })
     // }, []);
+
+
+    // -------- from firebase -------- 
+    useEffect(() => {
+        const fetchItem = async () => {
+          const itemCollection = await db.collection("item")
+          .where("id", "==", `${resultId}`)
+          .get();
+          setItem(
+            itemCollection.docs.map((doc) => {
+              return doc.data();
+            })
+          );
+        };
+        fetchItem();
+    }, []);
+      
+    if (item.length > 0) {
+        console.log("specific item: ", item);
+      }
     // handleInputChange() {
+        //LocalStorage Fun
     // }
+
     // handleFormSubmit() {
+
     // }
     return (
         <div className="container-fluid has-background-grey-light full">
@@ -27,20 +66,34 @@ function ItemDetails() {
                 <div className="column has-background-grey-light">
                     <div className="columns">
                         <div className="column is-one-quarter">
-                            <img alt="placeholder" className="item-image" src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=480&w=320" />
+                            {item.length > 0 &&
+                                <img alt={item[0].name} className="item-image" src={item[0].pic} />
+                            } 
                         </div>
                         <div className="column is-three-quarters has-background-grey">
-                            <ul>
-                                <li class="title is-4">Item Name:</li>
-                                <button class="title is-5"> Small</button>
-                                <button class="title is-5"> Medium</button>
-                                <button class="title is-5"> Large</button>
-                                <li><NavLink class="title is-5" to="/cart">Add to Cart</NavLink></li>
-                                <div className="level"></div>
-                                <div className="level"></div>
-                                <li class="title is-4">Creator Info:</li>
-                                <li class="title is-4">About item:</li>
-                            </ul>
+                            
+                                
+                                <ul>
+                                    {item.length > 0 &&
+                                        <li className="title is-4">{item[0].name}</li>
+                                    }
+                                    <form className="control">
+                                        <input type="radio" name="size" value="small" /> 
+                                        <label for="small"> Small </label>
+                                        <input type="radio" name="size" value="medium" />
+                                        <label for="medium"> Medium </label>
+                                        <input type="radio" name="size" value="large" /> 
+                                        <label for="large"> Large </label>
+                                        {/* className="title is-5 */}
+                                    </form>
+                                    
+                                    <li><Link className="title is-5" to="/cart">Add to Cart</Link></li>
+                                    <div className="level"></div>
+                                    <div className="level"></div>
+                                    <li className="title is-4">Creator Info:</li>
+                                    <li className="title is-4">About item:</li>
+                                </ul>
+                            
                         </div>
                     </div>
                 </div>
