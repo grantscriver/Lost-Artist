6d8fc2052714c713f72585b27d818a54d642f24c
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-15 19:29:37 -0600

    refactored code to deconstruct name, email, and picture from user

--- client/src/components/Profiles/Profile.js
+++ client/src/components/Profiles/Profile.js
@@ -4,6 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Profile = () => {
   const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
+  const { name, picture, email } = user;
 
   useEffect(() => {
     const getUserMetadata = async () => {
@@ -37,9 +38,9 @@ const Profile = () => {
   return (
     isAuthenticated && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <img src={picture} alt={name} />
+        <h2>{name}</h2>
+        <p>{email}</p>
         <h3>User Metadata</h3>
         {console.log(user)}
         {userMetadata ? (
