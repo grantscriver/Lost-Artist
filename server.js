5dc154c1f129d853ac3a5ef23ba3a5573a57c2a3
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-15 19:35:12 -0600

    starting fresh with server.js -- added npm package 'cors'

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 require("dotenv").config();
+
+// Required External Modules
 const express = require('express');
+const cors = require("cors");
+const helmet = require("helmet");
 const bodyParser = require('body-parser');
 const app = express();
-const { auth, requiresAuth } = require('express-openid-connect');
 const PORT = process.env.PORT || 8080;
 const auth0Config = require("./config/auth0.config");
-const jwt = require('express-jwt');
-const jwtAuthz = require('express-jwt-authz');
-const jwksRsa = require('jwks-rsa');
-const checkScopes = jwtAuthz([ 'read:messages' ]);
 let db = require("./models");
 
 
@@ -16,28 +15,6 @@ let db = require("./models");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(auth(auth0Config));
-
-
-// Authorization middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
-const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and 
-  // the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${process.env.AUTH0_ISSUER}.well-known/jwks.json`
-  }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_ISSUER,
-  algorithms: ['RS256']
-});
-
 
 
 // ROUTES
@@ -50,18 +27,6 @@ app.get('/api/public', function(req, res) {
 });
 
 // This route needs authentication
-app.get('/api/private', checkJwt, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-  });
-});
-
-
-app.get('/api/private-scoped', checkJwt, checkScopes, function(req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-  });
-});
 
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
