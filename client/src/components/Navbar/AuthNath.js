5d6e3496255097fd9459fd6f737df124711e8e70
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-15 19:34:19 -0600

    created navbar-item that imports AuthenticationButton -- AuthNav gets imported into Navbar.js

---
+++ client/src/components/Navbar/AuthNath.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AuthenticationButton from "../Buttons/Authentication-Button";
+
+const AuthNav = () => (
+    <div className="navbar-item">
+        <AuthenticationButton />
+    </div>
+);
+
+export default AuthNav;
