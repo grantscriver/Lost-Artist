b0d0f4ce012395b39d55ad31257237fae072d536
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-15 19:35:51 -0600

    moved files into auth folder

--- client/src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-const BASEURL = "https://api.unsplash.com/search/photos?page=1&query="
-
-export default {
-    searchPhotos: function (searchTerm) {
-        return axios.get(BASEURL + searchTerm);
-    }
-};
