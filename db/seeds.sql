c7494c4fbf6794e7a7963a1e148894a0d9fc8820
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-18 16:50:43 -0600

    updated seed file to match database models

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,11 +9,11 @@ VALUES("IA", "Iowa"),
 ("WI", "Wisconsin");
 
 
-INSERT INTO artists (artist_name, artist_email, artist_instagram, stateId)
-VALUES ("Mal Ludwig", "mal_ludwig4@email.com", "@ludiwgmal", 3),
-("Joe Johnson", "joeyjohnson@email.com", "@joesclothes", 1),
-("Monica Haverford", "haverford_m@email.com", "@stylehaverford", 2);
-
+INSERT INTO artists (artist_name, artist_email, artist_instagram, artist_city, artist_about, stateId)
+VALUES ("Mal Ludwig", "mal_ludwig4@email.com", "@ludiwgmal", "Battle Creek", "Hi, my name is Mal and I love creating unique designs!", 3),
+("Joe Johnson", "joeyjohnson@email.com", "@joesclothes", "Des Moines", "I'm Joe. Be sure to check out my instagram to see all my upcoming pieces for my collection.", 1),
+("Monica Haverford", "haverford_m@email.com", "@stylehaverford", "Alexandria", "All my designs are lake themed.", 2),
+("Calvin Freese", "calvin.freese@hotmail.com", "@whacking_mufn", "Minneapolis", "Help wanted, apply within.", 2);
 
 
 INSERT INTO items (style_name, category, color, state_abbr, price, description, artistId)
