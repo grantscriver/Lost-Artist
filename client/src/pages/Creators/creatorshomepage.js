32b1c81a4f9cd06217463dfc572539bb10f1364a
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-19 00:03:11 -0600

    added filtering for state drop downs

--- client/src/pages/Creators/creatorshomepage.js
+++ client/src/pages/Creators/creatorshomepage.js
@@ -1,158 +1,144 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
-import Hero from "../../components/Hero/Hero";
+import { StateDropDown } from "../../components/Forms/EditCreatorfile";
 import CreatorColumn from "../../components/column/CreatorColumn";
 import axios from 'axios';
 import Wrapper from '../../components/Wrapper/Wrapper';
 
 function Creator () {
     const [creators, setCreators] = useState([]);
+    const [filter, setFilter] = useState([creators]);
 
     useEffect(() => {
         axios.get("/api/creators")
         .then(res => {
-            console.log(res.data);
             setCreators(res.data);
         })
     }, [])
+
+
+    function handleInputChange(event) {
+        let value = event.target.value;
+        let numVal = parseInt(value);
+        setFilter([])
+        if (numVal > 0) {
+            creators.filter(creator => creator.stateId === numVal).map(filteredCreator => (
+                setFilter(filter => [...filter, filteredCreator])
+            ))
+        } else {
+            setFilter(creators)
+        }
+        
+        
+    }
+    console.log(filter.length);
+
     return (
         <>
-            
-                
-                <div className="dropdown is-hoverable">
-                        <div className="dropdown-trigger">
-                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                                <span>State</span>
-                                <span className="icon is-small">
-                                    <i className="fas fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </button>
-                        
-                            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                                <div className="dropdown-content">
-                                    <a href="#" className="dropdown-item">
-                                        MN
-                                    </a>
-                                    <hr className="dropdown-divider"></hr>
-                                    <a className="dropdown-item">
-                                        ND
-                                    </a>
-                                    <hr className="dropdown-divider"></hr>
-                                    <a href="#" className="dropdown-item">
-                                        SD
-                                    </a>
-                                    <hr className="dropdown-divider"></hr>
-                                    <a href="#" className="dropdown-item">
-                                        WI
-                                    </a>
-                                    <hr className="dropdown-divider"></hr>
-                                    <a href="#" className="dropdown-item">
-                                        IA
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-                    
+        <div className="level">
+                <form className="level-item">
+                    <StateDropDown onChange={handleInputChange} />
+                </form>
+        </div>
 
-                <div className="columns">
-                    
-                    <div className="column is-one-fifth has-background-white">
-                        <aside className="menu has-text-centered">
-                            <p className="menu-label">
-                                IA
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 1) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            <p className="menu-label">
-                                MN
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 2) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            <p className="menu-label">
-                                MI
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 3) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            <p className="menu-label">
-                                ND
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 4) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            <p className="menu-label">
-                                SD
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 5) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            <p className="menu-label">
-                                WI
-                            </p>
-                                <ul className="menu-list">
-                                {creators.length > 0 &&
-                                     creators.map((creator, i) => {
-                                         if(creator.stateId === 6) {
-                                            return <li><a href={`/creators/${creator.id}`}>{creator.artist_name}</a></li>
-                                         }
-                                    })
-                                }
-                                </ul>
-                            </aside>
-                    </div>
-                    <Wrapper>
-                    {creators.length > 0 && 
-                        creators.map((creator, i) => {
-                            console.log(creator);
-                            return (
-                        <Link to={`/creators/${creator.id}`}> 
-                                             
-                            <CreatorColumn {...creator} />
-                        </Link>
-                            )
-                        })
-                    }
-                    </Wrapper>
-                </div>
-            
-            
-            
-            
+        <div className="columns">
                 
-        </>
+            <div className="column is-one-fifth has-background-white">
+                <aside className="menu has-text-centered">
+                    <p className="menu-label">
+                        IA
+                    </p>
+                        <ul className="menu-list">
+                        {creators.length > 0 &&
+                                creators.map((creator, i) => {
+                                    if(creator.stateId === 1) {
+                                        return <li key={creator.id}><Link to={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                    } else {
+                                        return null
+                                    }
+                            })
+                        }
+                        </ul>
+                    <p className="menu-label">MN</p>
+                        <ul className="menu-list">
+                        {creators.length > 0 &&
+                                creators.map((creator, i) => {
+                                    if(creator.stateId === 2) {
+                                        return <li key={creator.id}><Link to={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                    } else {
+                                        return null
+                                    }
+                            })
+                        }
+                        </ul>
+                    <p className="menu-label">MI</p>
+                        <ul className="menu-list">
+                            {creators.length > 0 &&
+                                    creators.map((creator, i) => {
+                                        if(creator.stateId === 3) {
+                                            return <li key={creator.id}><Link to={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                        } else {
+                                            return null
+                                        }
+                                })
+                            }
+                        </ul>
+                    <p className="menu-label">ND</p>
+                        <ul className="menu-list">
+                            {creators.length > 0 &&
+                                    creators.map((creator, i) => {
+                                        if(creator.stateId === 4) {
+                                            return <li key={creator.id}><Link to={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                        } else {
+                                            return null
+                                        }
+                                })
+                            }
+                        </ul>
+                    <p className="menu-label">SD</p>
+                        <ul className="menu-list">
+                            {creators.length > 0 &&
+                                    creators.map((creator, i) => {
+                                        if(creator.stateId === 5) {
+                                            
+                                            return <li key={creator.id}><Link to={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                        } else {
+                                            return null
+                                        }
+                                })
+                            }
+                        </ul>
+                    <p className="menu-label">WI</p>
+                        <ul className="menu-list">
+                            {creators.length > 0 &&
+                                    creators.map((creator, i) => {
+                                        if(creator.stateId === 6) {
+                                            return <li key={creator.id}><Link href={`/creators/${creator.id}`}>{creator.artist_name}</Link></li>
+                                        } else {
+                                            return null
+                                        }
+                                    })
+                            }
+                        </ul>
+                    </aside>
+            </div>
+            
+ 
+            <Wrapper>
+                {filter.length > 0 && 
+                    filter.map((creator, i) => {
+                        return (
+                    <Link key={creator.id} to={`/creators/${creator.id}`}> 
+                                            
+                        <CreatorColumn  {...creator} />
+                    </Link>
+                        )
+                    })
+                }
+            </Wrapper>
+
+        </div>
+    </>
     )   
 }
 
