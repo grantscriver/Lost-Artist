f1e6bfb388f1936f38ad2d738575b73d409972a1
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-19 17:02:30 -0600

    broke long text out into components & added query params API call ... and added nodemon as package

---
+++ client/src/components/Card/ItemCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ItemCard({id, image, name}) {
+    return (
+        <div className="column is-3" key={id}>
+            <div className="card">
+                <Link to={`/shop/item/${id}`}>
+                    <div className="card-image">
+                        <figure className="image is-3by4">
+                            <img src={image} alt={name} />
+                        </figure>
+                    </div>
+                    <div className="card-content">
+                        <div className="media">
+                            <div className="media-content">
+                                <p className="title is-4">{name}</p>
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+
+export default ItemCard;
--- client/src/pages/Creators/editcreator.js
+++ client/src/pages/Creators/editcreator.js
@@ -8,16 +8,16 @@ function Editcreator() {
   // db
   const [creator, setCreator] = useState([]);
   // auth0
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
+  const { user, getAccessTokenSilently } = useAuth0();
+  
   //form
   const [form, setForm] = useState({});
   // env
-  const serverUrl = process.env.REACT_APP_SERVER_URL;
+  
   let URL = window.location.pathname;
-    let search = URL.lastIndexOf("/");
-    let resultId = URL.substring(search + 1);
-    console.log(resultId);
+  let search = URL.lastIndexOf("/");
+  let resultId = URL.substring(search + 1);
+    
   
   useEffect(() => {
     const getUserMetadata = async () => {
@@ -29,17 +29,15 @@ function Editcreator() {
           scope: "read:current_user",
         });
   
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+        // const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
   
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
+        // const metadataResponse = await fetch(userDetailsByIdUrl, {
+        //   headers: {
+        //     Authorization: `Bearer ${accessToken}`,
+        //   },
+        // });
   
-        const { user_metadata } = await metadataResponse.json();
-  
-        setUserMetadata(user_metadata);
+        
       } catch (e) {
 
         console.log(e.message);
