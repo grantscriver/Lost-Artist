11c1218db3cf8ca66c1435821442a854f3020be7
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-19 17:43:07 -0600

    adjusted props name to style_name

--- client/src/components/Card/ItemCard.js
+++ client/src/components/Card/ItemCard.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function ItemCard({id, image, name}) {
+function ItemCard({id, image, style_name}) {
     return (
         <div className="column is-3" key={id}>
             <div className="card">
                 <Link to={`/shop/item/${id}`}>
                     <div className="card-image">
                         <figure className="image is-3by4">
-                            <img src={image} alt={name} />
+                            <img src={image} alt={style_name} />
                         </figure>
                     </div>
                     <div className="card-content">
                         <div className="media">
                             <div className="media-content">
-                                <p className="title is-4">{name}</p>
+                                <p className="title is-4">{style_name}</p>
                             </div>
                         </div>
                     </div>
