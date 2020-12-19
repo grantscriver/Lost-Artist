bf02308c9f58d59e819a9f421d1e984cd1f3bee9
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-19 17:46:27 -0600

    refactored code to use components and updated page to display info from database :sparkles:

--- client/src/pages/Creators/publicprofile.js
+++ client/src/pages/Creators/publicprofile.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Wrapper from "../../components/Wrapper/Wrapper";
+import {CategorySelect, ColorSelect} from "../../components/Forms/DropSelect";
+import { CreatorInfo } from "../../components/column/CreatorAbout";
+import ItemCard from "../../components/Card/ItemCard";
 import axios from "axios";
 
-import { app } from "../../base";
 
-const db = app.firestore();
+
+
 
 function  PublicProfile() {
     const [creator, setCreator] = useState({});
-    const [item, setItem] = useState([]);
-
-    const { id, artist_name, artist_email, artist_instagram, artist_about, artist_city, artist_state } = creator;
+    const [items, setItems] = useState([]);
+    const { id } = creator;
 
     let URL = window.location.pathname;
     let search = URL.lastIndexOf("/");
@@ -21,27 +22,19 @@ function  PublicProfile() {
     useEffect(() => {
         axios.get(`/api/public/creator/${resultId}`)
         .then(res => {
-            console.log("HELLO?")
             console.log(res.data)
             setCreator(res.data);
             transitionIdToState(res.data.stateId);
-        })
-        const fetchItem = async () => {
-            // Change this query to item specific when database is ready            
-            const itemCollection = await db.collection("item").get();
-            setItem(
-                itemCollection.docs.map((doc) => {
-                    return doc.data();
-                })
-            );
-        };
-        fetchItem();
-
-        
+            getItemsByUser(res.data.id);
+        })        
     }, []);
 
-    if (item.length > 0) {
-        console.log(item);
+    function getItemsByUser(id) {
+        axios.get(`/api/items/?artistId=${id}`)
+        .then(res => {
+            console.log(res.data);
+            setItems(res.data);
+        })
     }
 
     function transitionIdToState(id) {
@@ -69,48 +62,17 @@ function  PublicProfile() {
         }
         
   }
-
-
-
-
-
+  
     return (
         <>
             <div className="card" >
                 <div className="columns">
                     <div className="column is-3 is-offset-1">
-                        <img src="https://bulma.io/images/placeholders/480x640.png" alt="placeholder" />
+                        <img src={creator.image || "https://bulma.io/images/placeholders/480x640.png"} alt="placeholder" />
                     </div>
                     {creator.artist_name 
-                        ? (
-                        <div className="column is-5">
-                            <p className="title is-4">{artist_name}</p>
-                            <p className="title is-5">{artist_city}, {artist_state} </p>
-                            <br></br>
-
-                            <p>{artist_about}</p>
-
-                            <div className="level"></div>
-                            <div className="level"></div>
-                            <div className="level"></div>
-                            <div className="level"></div>
-                            <div className="columns ">
-                                <div className="column is-one-quarter">
-                                    <h1 className="title is-4">Contact:</h1>
-                                </div>
-                                <div className="column is-one-quarter is-offset-one-half">
-                                    <a className="icon" href={`https://instagram.com/${artist_instagram}`}>
-                                        <i class="fab fa-instagram fa-3x"></i>
-                                    </a>
-
-                                    <a className="icon is-pulled-right" href={`mailto:${artist_email}`}>
-                                        <i class="far fa-envelope fa-3x"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                        : null
+                        ? <CreatorInfo {...creator} />
+                        : <p>This creator has no info to display.</p>
 
                     }
                 </div>
@@ -119,66 +81,11 @@ function  PublicProfile() {
             <div className="level"></div>
             <div className="columns">
                 <div className="space-left column is-two-thirds">
-    
                     {/* placeholder column to push Cat & Color dropdowns right */}
                 </div>
                 <div className="column is-justify-content-space-around">
-                    <div className="dropdown  is-hoverable">
-                        <div className="dropdown-trigger">
-                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                                <span>Category</span>
-                                <span className="icon is-small">
-                                    <i className="fas fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </button>
-                        </div>
-                        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                            <div className="dropdown-content">
-                                <a href="#" className="dropdown-item">
-                                    All
-                                    </a>
-                                <a href="#" className="dropdown-item">
-                                    Hats
-                                    </a>
-                                <a href="#" className="dropdown-item">
-                                    Shirts
-                                    </a>
-                                <a href="#" className="dropdown-item">
-                                    Hoodies
-                                    </a>
-                                <a href="#" className="dropdown-item">
-                                    Shoes
-                                    </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="space-left dropdown is-hoverable">
-                        <div className="dropdown-trigger">
-                            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
-                                <span>Color</span>
-                                <span className="icon is-small">
-                                    <i className="fas fa-angle-down" aria-hidden="true"></i>
-                                </span>
-                            </button>
-                        </div>
-                    
-                        <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                            <div className="dropdown-content">
-                                <a href="#" className="dropdown-item">
-                                    White
-                                        </a>
-                                <a href="#" className="dropdown-item">
-                                    Black
-                                        </a>
-                                <a href="#" className="dropdown-item">
-                                    Grey
-                                        </a>
-                                <a href="#" className="dropdown-item">
-                                    Beige
-                                        </a>
-                            </div>
-                        </div>
-                    </div>
+                    <CategorySelect />
+                    <ColorSelect spacing={"space-left"}/>
                 </div>
             </div>
             <div className="level"></div>
@@ -186,28 +93,10 @@ function  PublicProfile() {
             <section className="space-left">
                 <div className="columns is-flex-justify-content-center">
                     <Wrapper>
-                        {item.length > 0 &&
-                            item.map((item) => {
-                                return (
-                                    <div className="column is-3" key={item.id}>
-                                        <div className="card">
-                                            <Link to={`/shop/item/${item.id}`}>
-                                                <div className="card-image">
-                                                    <figure className="image is-3by4">
-                                                        <img src={item.pic} alt={item.name} />
-                                                    </figure>
-                                                </div>
-                                                <div className="card-content">
-                                                    <div className="media">
-                                                        <div className="media-content">
-                                                            <p className="title is-4">{item.name}</p>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                );
+                        {items.length > 0 &&
+                            items.map((item) => {
+                                return <ItemCard key={id}{...item} />
+                                
                             })}
                     </Wrapper>
                 </div>
