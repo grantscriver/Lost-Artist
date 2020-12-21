890ccb4a5e997f48b067c4c51f1dd74d4214fddd
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-12-21 15:11:52 -0600

    testing something out (commented out res.sendFile in server.js for production)

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   
-  app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  });
+  // app.get("*", function(req, res) {
+  // res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  // });
 }
 
 if (process.env.JAWSDB_URL) {
